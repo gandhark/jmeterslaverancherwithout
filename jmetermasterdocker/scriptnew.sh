e6d8f08c16658c01ca4db77385b3bc40f8f6a23c
abhijeetka <abhijeetka@cybage.com>
2015-10-06 17:35:11 +0530

    changes

---
+++ jmetermasterdocker/scriptnew.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+#export host=$2
+export workspace=$1
+#export users=$3
+#export count=$4
+#export hostip=$5
+
+docker pull www.cybage-docker-registry.com:9080/jmeterslave
+docker pull www.cybage-docker-registry.com:9080/jmetermaster
+
+
+#Run docker slave container1
+docker run --name jmeterslave1 -d www.cybage-docker-registry.com:9080/jmeterslave
+
+#Run docker container2
+docker run --name jmeterslave2 -d www.cybage-docker-registry.com:9080/jmeterslave
+
+
+
+export a=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' jmeterslave1 )
+export b=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' jmeterslave2 )
+export host=dev.alm-task-manager.com
+#docker build -t jmetergenericmaster .
+#docker build -t jmetergenericmaster .
+docker run --name jmetergenericmaster -d -v /reports:/reports -e IP=$a,$b  jmetermaster
+ls /reports
+
+cp /reports/reportnew.xml  $1
+
+
+
+
