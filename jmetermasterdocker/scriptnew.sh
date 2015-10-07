45c0bd6243ce6fa5d359997a5f37e38be5a99e8b
abhijeetka <abhijeetka@cybage.com>
2015-10-07 15:28:57 +0530

    master jmeter added workspace for volume mounting

--- jmetermasterdocker/scriptnew.sh
+++ jmetermasterdocker/scriptnew.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-
-#export host=$2
 export workspace=$1
-#export users=$3
-#export count=$4
-#export hostip=$5
 
 docker pull www.cybage-docker-registry.com:9080/jmeterslave
 docker pull www.cybage-docker-registry.com:9080/jmetermaster
@@ -22,13 +17,9 @@ docker run --name jmeterslave2 -d www.cybage-docker-registry.com:9080/jmeterslav
 export a=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' jmeterslave1 )
 export b=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' jmeterslave2 )
 export host=dev.alm-task-manager.com
-#docker build -t jmetergenericmaster .
-#docker build -t jmetergenericmaster .
-docker run --name jmetermaster -d -v /reports:/reports -e IP=$a,$b  www.cybage-docker-registry.com:9080/jmetermaster
 
-ls /reports
+docker run --name jmetermaster -d -v $workspace:/reports -e IP=$a,$b  www.cybage-docker-registry.com:9080/jmetermaster
 
-cp /reports/reportnew.xml  $workspace
 
 
 
