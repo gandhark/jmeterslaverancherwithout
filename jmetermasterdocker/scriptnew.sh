8097744f2849287e1a8d8e62291b896ade1e59de
Prakash Suvarnkar <prakashs@evolvingsols.com>
2015-10-09 11:05:21 +0530

    modified scriptnew

--- jmetermasterdocker/scriptnew.sh
+++ jmetermasterdocker/scriptnew.sh
@@ -6,19 +6,22 @@ docker pull www.cybage-docker-registry.com:9080/jmeterslave
 docker pull www.cybage-docker-registry.com:9080/jmetermaster
 
 
-#Run docker slave container1
-docker run --name jmeterslave1 -d www.cybage-docker-registry.com:9080/jmeterslave
 
-#Run docker container2
-docker run --name jmeterslave2 -d www.cybage-docker-registry.com:9080/jmeterslave
+for(i=0;i<$2;i++)
+do
+
+#Run docker slave container$i
+docker run --name jmeterslave$i -d www.cybage-docker-registry.com:9080/jmeterslave
 
 
 echo "fetching slave containers IP and storing it into variable a and variable b";
-export a=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' jmeterslave1 )
-export b=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' jmeterslave2 )
+export ip$i=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' jmeterslave1 )
 export host=dev.alm-task-manager.com
 
-docker run --name jmetermaster -d -v $workspace:/reports -e IP=$a,$b  www.cybage-docker-registry.com:9080/jmetermaster
+done;
+
+
+#docker run --name jmetermaster -d -v $workspace:/reports -e IP=$a,$b  www.cybage-docker-registry.com:9080/jmetermaster
 
 
 
