9e260c8cb9229b2fd66c684228d172cb584e870e
Prakash Suvarnkar <prakashs@evolvingsols.com>
2015-10-09 11:43:15 +0530

    changes

--- jmetermasterdocker/scriptnew.sh
+++ jmetermasterdocker/scriptnew.sh
@@ -7,20 +7,37 @@ docker pull www.cybage-docker-registry.com:9080/jmetermaster
 
 echo $2;
 
-for (( i=1; i <= $number; i++ ))
 
-#for i in {1 -- $number}
+
+
+#echo "fetching slave containers IP and storing it into variable a and variable b";
+#export ip$i=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' jmeterslave1 )
+#export host=dev.alm-task-manager.com
+
+#done;
+
+
+
+a=0
+
+while [ $a -lt $2 ]
 do
-echo $i;
-#Run docker slave container$i
-docker run --name jmeterslave$i -d www.cybage-docker-registry.com:9080/jmeterslave
+  echo $a
+  docker run --name jmeterslave$a -d www.cybage-docker-registry.com:9080/jmeterslave
+
+   a=`expr $a + 1`
 
 
-echo "fetching slave containers IP and storing it into variable a and variable b";
-export ip$i=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' jmeterslave1 )
-export host=dev.alm-task-manager.com
+#echo "fetching slave containers IP and storing it into variable a and variable b";
+#export ip$i=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' jmeterslave1 )
+#export host=dev.alm-task-manager.com
 
-done;
+done
+
+
+
+
+
 
 
 #docker run --name jmetermaster -d -v $workspace:/reports -e IP=$a,$b  www.cybage-docker-registry.com:9080/jmetermaster
